95fc9a3d3796ef9d150175e89b76269f8efd48bb
Shawn Campbell <scampbell@vetsfirstchoice.com>
2015-07-21 23:48:20 -0400

    initial commit

---
+++ Mac/TurnKitMac/TurnKit.swift
@@ -0,0 +1,9 @@
+//
+//  TurnKit.swift
+//  
+//
+//  Created by Shawn Campbell on 7/21/15.
+//
+//
+
+import Foundation
---
+++ Mac/TurnKitMac/TurnKitMac/TurnKitMac.h
@@ -0,0 +1,19 @@
+//
+//  TurnKitMac.h
+//  TurnKitMac
+//
+//  Created by Shawn Campbell on 7/21/15.
+//  Copyright © 2015 Team Awesome McPants. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TurnKitMac.
+FOUNDATION_EXPORT double TurnKitMacVersionNumber;
+
+//! Project version string for TurnKitMac.
+FOUNDATION_EXPORT const unsigned char TurnKitMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TurnKitMac/PublicHeader.h>
+
+
