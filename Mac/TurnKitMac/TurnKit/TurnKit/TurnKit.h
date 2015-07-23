66a7746bfe5ee96e87529e729ab2289f124e4dbe
Kyle Kilgour <kkilgour@cashstar.com>
2015-07-23 14:25:42 -0400

    Updated to single project with multiple targets.

---
+++ Mac/TurnKitMac/TurnKit/TurnKit/TurnKit.h
@@ -0,0 +1,19 @@
+//
+//  TurnKit.h
+//  TurnKit
+//
+//  Created by Kyle Kilgour on 7/23/15.
+//  Copyright (c) 2015 TeamAwesomeMcPants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TurnKit.
+FOUNDATION_EXPORT double TurnKitVersionNumber;
+
+//! Project version string for TurnKit.
+FOUNDATION_EXPORT const unsigned char TurnKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TurnKit/PublicHeader.h>
+
+
---
+++ TurnKit/TurnKitiOS/TurnKitiOS.h
@@ -0,0 +1,19 @@
+//
+//  TurnKitiOS.h
+//  TurnKitiOS
+//
+//  Created by Kyle Kilgour on 7/23/15.
+//  Copyright (c) 2015 TeamAwesomeMcPants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TurnKitiOS.
+FOUNDATION_EXPORT double TurnKitiOSVersionNumber;
+
+//! Project version string for TurnKitiOS.
+FOUNDATION_EXPORT const unsigned char TurnKitiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TurnKitiOS/PublicHeader.h>
+
+
